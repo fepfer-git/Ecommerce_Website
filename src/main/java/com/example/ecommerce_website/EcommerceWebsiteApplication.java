89f58151ca7180305a83a953a6f385cecb1be440
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-10-15 16:01:41 +0700

    Test

--- src/main/java/com/example/ecommerce_website/EcommerceWebsiteApplication.java
+++ src/main/java/com/example/ecommerce_website/EcommerceWebsiteApplication.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceWebsiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceWebsiteApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommerceWebsiteApplication.class, args);
-	}
+
+        System.out.println("Hello World");
+        System.out.println("hihihi");
+    }
+
+
 
 }
--- src/test/java/com/example/ecommerce_website/EcommerceWebsiteApplicationTests.java
+++ src/test/java/com/example/ecommerce_website/EcommerceWebsiteApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EcommerceWebsiteApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
