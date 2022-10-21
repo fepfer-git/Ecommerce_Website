f3bc09db0258f38c076126d225a9ce94cf020e06
Nguyễn Minh Thái <nguyenminhthai3108@gmail.com>
2022-10-21 10:49:51 +0700

    Add Validation for userDTO, check the length of UserId, password, and Email format. Handle validation message

--- src/main/java/com/example/ecommerce_website/dto/create/UserDtoCreate.java
+++ src/main/java/com/example/ecommerce_website/dto/create/UserDtoCreate.java
@@ -1,6 +1,7 @@
 package com.example.ecommerce_website.dto.create;
 
-import lombok.Data;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -8,12 +9,17 @@ import javax.validation.constraints.NotBlank;
 /**
  * A DTO for the {@link com.example.ecommerce_website.entity.User} entity
  */
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDtoCreate {
+    @Length(min = 3, max = 15, message = "User Id must be between 3-15 characters!")
     @NotBlank(message = "User Id cannot be blank!")
     private String userId;
     @NotBlank(message = "Name cannot be blank!")
     private String name;
+    @Length(min = 6, max = 20, message = "Password must be between 6-20 characters!")
     @NotBlank(message = "Password cannot be blank!")
     private String password;
     @Email(message = "Email is not valid!")
--- src/main/java/com/example/ecommerce_website/exception/CustomerExceptionHandler.java
+++ src/main/java/com/example/ecommerce_website/exception/CustomerExceptionHandler.java
@@ -4,15 +4,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.ZonedDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CustomerExceptionHandler {
     @ExceptionHandler(value = NotFoundException.class)
     public ResponseEntity<Object> handleNotFoundException(NotFoundException ex){
